1a70ad732b0566a5e75df7174144971a78e7bfb7
Clif Smith <clif@texicans.us>
2012-08-30 16:51:28 -0500

    Report data bag items and roles and they're being uploaded

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email  "clif@texicans.us"
 license           "Apache 2.0"
 description       "Uses cookbooks, databags and roles from Git and uploads them to Chef"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.2.0"
+version           "0.2.1"
 
 depends "logrotate"
